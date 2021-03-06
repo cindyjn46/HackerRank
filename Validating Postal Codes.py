317a43a654d5a409f6d827fa3b5f428da573946c
cindyjn46 <cindyjn46@gmail.com>
2021-03-06 14:48:15 +0800

    0306

---
+++ Validating Postal Codes.py
@@ -0,0 +1,13 @@
+#121426 # Here, 1 is an alternating repetitive digit.
+523563 # Here, NO digit is an alternating repetitive digit.
+552523 # Here, both 2 and 5 are alternating repetitive digits.
+
+regex_integer_in_range = r'^[1-9][\d]{5}$'	# Do not delete 'r'.# 100000 - 999999
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	# Do not delete 'r'.
+
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)

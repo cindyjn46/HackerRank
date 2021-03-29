8226fac86746c96c777f928c090b62e51531f749
cindyjn46 <cindyjn46@gmail.com>
2021-03-29 11:08:11 +0800

    Create Re.findall() & Re.finditer().py

---
+++ Re.findall() & Re.finditer().py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re
+# vowel >2 
+Storage = re.findall(r'(?<=[qwrtypsdfghjklzxcvbnm])([aeiou]{2,})(?=[qwrtypsdfghjklzxcvbnm])', input().strip(), re.IGNORECASE)
+
+if Storage:
+    for i in Storage:
+        print(i)
+else:
+    print(-1)
